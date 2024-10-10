2fe1a0c90e05ca2048f71e7e96da657641247719
Mauritz Sverredal <23345144+Marre3@users.noreply.github.com>
2024-10-10 14:45:28 +0200

    Refactor the scripts

--- Department_Script.py
+++ Department_Script.py
@@ -1,36 +1,59 @@
+from contextlib import closing
+from os import getenv
 import pymysql
 from sshtunnel import SSHTunnelForwarder
 
-tunnel = SSHTunnelForwarder(
-    ('fries.it.uu.se', 22),
-    ssh_username='USERNAME',
-    ssh_password='PASSWORD',
-    remote_bind_address=('127.0.0.1', 3306)
-)
-tunnel.start()
 
-connection = pymysql.connect(
-    host='127.0.0.1',
-    user='ht24_1_group_35',
-    password='pasSWd_35',
-    port=tunnel.local_bind_port,
-    database='ht24_1_project_group_35'
-)
-
-def list_items(department_id):
-    cur = connection.cursor()
-    cur.execute("SELECT COUNT(*) FROM Department WHERE Parent_ID = %s", (department_id,))
+def list_items(conn, department_id):
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT COUNT(*) FROM Department WHERE Parent_ID = %s",
+        (department_id,)
+    )
     if cur.fetchone()[0] == 0:
-        cur.execute("SELECT Product_ID, Product_Title, (Price_without_VAT * (1 - Sale_percentage / 100)) FROM Product WHERE Department_ID = %s", (department_id,))
+        cur.execute(
+            (
+                "SELECT Product_ID, Product_Title, "
+                "(Price_without_VAT * (1 - Sale_percentage / 100)) "
+                "FROM Product WHERE Department_ID = %s"
+            ),
+            (department_id,)
+        )
         for row in cur.fetchall():
             print(row)
     else:
-        cur.execute("SELECT Department_ID, Description FROM Department WHERE Parent_ID = %s", (department_id,))
+        cur.execute(
+            (
+                "SELECT Department_ID, Description "
+                "FROM Department WHERE Parent_ID = %s"
+            ),
+            (department_id,)
+        )
         for row in cur.fetchall():
             print(row)
     cur.close()
 
-list_items(int(input("Enter the department ID: ")))
 
-connection.close()
-tunnel.stop()
+with closing(
+    SSHTunnelForwarder(
+        ('fries.it.uu.se', 22),
+        ssh_username=getenv("uu_username"),  # use your Studium username
+        ssh_password=getenv("sqlpw"),  # use your Studium password
+        remote_bind_address=('127.0.0.1', 3306)
+    )
+) as tunnel:
+    tunnel.start()
+
+    with closing(
+        pymysql.connect(
+            host='127.0.0.1',
+            user='ht24_1_group_35',
+            password='pasSWd_35',
+            port=tunnel.local_bind_port,
+            database='ht24_1_project_group_35'
+        )
+    ) as connection:
+        id = 0
+        while id != -1:
+            id = int(input("Enter the department ID: "))
+            list_items(connection, id)
--- Product_Script.py
+++ Product_Script.py
@@ -1,37 +1,52 @@
+from contextlib import closing
+from os import getenv
 import pymysql
 from sshtunnel import SSHTunnelForwarder
 
-tunnel = SSHTunnelForwarder(
-    ('fries.it.uu.se', 22),
-    ssh_username='USERNAME',
-    ssh_password='PASSWORD',
-    remote_bind_address=('127.0.0.1', 3306)
-)
-tunnel.start()
 
-connection = pymysql.connect(
-    host='127.0.0.1',
-    user='ht24_1_group_35',
-    password='pasSWd_35',
-    port=tunnel.local_bind_port,
-    database='ht24_1_project_group_35'
-)
-
-def update_discount(product_id):
-    cur = connection.cursor()
-    cur.execute("SELECT Sale_percentage FROM Product WHERE Product_ID = %s", (product_id,))
+def update_discount(conn, product_id):
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT Sale_percentage FROM Product "
+        "WHERE Product_ID = %s",
+        (product_id,)
+    )
     current_discount = cur.fetchone()
     if current_discount:
         print(f"Current Discount: {current_discount[0]}")
         new_discount = float(input("Enter new discount: "))
-        cur.execute("UPDATE Product SET Sale_percentage = %s WHERE Product_ID = %s", (new_discount, product_id))
-        connection.commit()
+        cur.execute(
+            "UPDATE Product SET Sale_percentage = %s "
+            "WHERE Product_ID = %s",
+            (new_discount, product_id)
+        )
+        conn.commit()
         print("Discount updated")
     else:
         print("Product not found")
     cur.close()
 
-update_discount(int(input("Enter the product ID: ")))
 
-connection.close()
-tunnel.stop()
+with closing(
+    SSHTunnelForwarder(
+        ('fries.it.uu.se', 22),
+        ssh_username=getenv("uu_username"),  # use your Studium username
+        ssh_password=getenv("sqlpw"),  # use your Studium password
+        remote_bind_address=('127.0.0.1', 3306)
+    )
+) as tunnel:
+    tunnel.start()
+
+    with closing(
+        pymysql.connect(
+            host='127.0.0.1',
+            user='ht24_1_group_35',
+            password='pasSWd_35',
+            port=tunnel.local_bind_port,
+            database='ht24_1_project_group_35'
+        )
+    ) as connection:
+        id = 0
+        while id != -1:
+            id = int(input("Enter the product ID: "))
+            update_discount(connection, id)
